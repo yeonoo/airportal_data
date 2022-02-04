28954b5f6f34b1ed3ba191eac155b31d33af8a2b
yeonoo <ywoo818@gmail.com>
2022-02-04 16:40:55 +0900

    코드 정리

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
 # 크롬 데이터 스크래퍼 셋팅
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
 driver = webdriver.Chrome()
 driver.implicitly_wait(time_to_wait=5)
 
 # [로그인]
 # 1. 로그인 페이지 접속
-driver.get('https://www.airportal.go.kr/common/login/login01.jsp')
+url_login = 'https://www.airportal.go.kr/common/login/login01.jsp'
+driver.get(url_login)
 # 2. 로그인 정보 입력
 driver.find_element_by_name('df_userid').send_keys('yeonoo')
 driver.find_element_by_name('df_passwd').send_keys('lyw8485!')
 # 3. 로그인 버튼 클릭
-login_x_path = '/html/body/form/table[3]/tbody/tr[2]/td[2]/table[3]/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/input'
-driver.find_element_by_xpath(login_x_path).click()
+login_button = '/html/body/form/table[3]/tbody/tr[2]/td[2]/table[3]/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/input'
+driver.find_element_by_xpath(login_button).click()
 
 # 실시간 통계 페이지 접속
-url = 'https://www.airportal.go.kr/knowledge/statsnew/realtime/airlineRoute.jsp#'
-driver.get(url)
+url_realtime = 'https://www.airportal.go.kr/knowledge/statsnew/realtime/airlineRoute.jsp#'
+driver.get(url_realtime)
 
 # 제주 노선 설정
 from selenium.webdriver.support.select import Select
@@ -44,6 +43,3 @@ searchData = '//*[@id="realContents"]/div/div[2]/div[1]/div[1]/a[2]'
 driver.find_element_by_xpath(searchData).click()
 
 # 데이터 추출
-
-
-
