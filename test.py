baf8097694b13a4a7c8b9d891c082d24fe2b8fce
yeonoo <ywoo818@gmail.com>
2022-02-04 11:35:42 +0900

    날짜 지정 완료 리팩토링 필요

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 # 셋팅
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 driver = webdriver.Chrome()
 driver.implicitly_wait(time_to_wait=5)
 
 # 로그인
-driver.implicitly_wait(3)
 driver.get('https://www.airportal.go.kr/common/login/login01.jsp')
 login_x_path = '/html/body/form/table[3]/tbody/tr[2]/td[2]/table[3]/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/input'
 driver.find_element_by_name('df_userid').send_keys('yeonoo')
@@ -17,17 +18,26 @@ driver.get(url2)
 
 # 노선 설정
 from selenium.webdriver.support.select import Select
-select = Select(driver.find_element_by_name("seah_ad"))
-select.select_by_visible_text("제주")
-select.select_by_value("RKPC")
+selectRoute = Select(driver.find_element_by_name("seah_ad"))
+selectRoute.select_by_visible_text("제주")
+selectRoute.select_by_value("RKPC")
 
 # 날짜 지정
-# 날짜 지정
-from selenium.webdriver.common.by import By
-# driver.findElement(By.name("current_dt_from")).sendKeys("value", "20220201")
-# driver.findElement(By.name("current_dt_to")).sendKeys("value", "20220201")
-driver.find_element_by_id(("dateStart")).send_keys("value", "20220201")
-driver.find_element(By.id("dateEnd")).send_keys("value", "20220201")
+from selenium.webdriver.common.keys import Keys
+driver.find_element_by_name('current_dt_from').clear()
+driver.find_element_by_name('current_dt_from').send_keys("20220201")
+driver.find_element_by_name('current_dt_from').send_keys(Keys.ENTER)
+driver.find_element_by_name('current_dt_from').send_keys(Keys.BACKSPACE)
+driver.find_element_by_name('current_dt_from').send_keys("1")
+driver.find_element_by_name('current_dt_from').send_keys(Keys.ENTER)
+driver.find_element_by_name('current_dt_to').clear()
+driver.find_element_by_name('current_dt_to').send_keys("20220201")
+driver.find_element_by_name('current_dt_to').send_keys(Keys.ENTER)
+driver.find_element_by_name('current_dt_to').send_keys(Keys.BACKSPACE)
+driver.find_element_by_name('current_dt_to').send_keys("1")
+driver.find_element_by_name('current_dt_to').send_keys(Keys.ENTER)
+searchData = '//*[@id="realContents"]/div/div[2]/div[1]/div[1]/a[2]'
+driver.find_element_by_xpath(searchData).click()
 
 # 데이터
 
