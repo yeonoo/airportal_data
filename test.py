a6b87f651eac8dc94287813e573d21847b989284
yeonoo <ywoo818@gmail.com>
2022-02-10 16:51:05 +0900

    일단 마무리!

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 # [참조]
 from selenium import webdriver
 driver = webdriver.Chrome()
-# driver.implicitly_wait(time_to_wait=5)
+from selenium.webdriver.support.select import Select
+from datetime import date, timedelta
+import time
+
+# csv파일 생성
+import csv
+f = open('airportalData.csv', 'a', newline='')
+wr = csv.writer(f)
 
 # [로그인]
 # 1. 로그인 페이지 접속
@@ -14,28 +21,21 @@ driver.find_element_by_name('df_passwd').send_keys('lyw8485!')
 login_button = '/html/body/form/table[3]/tbody/tr[2]/td[2]/table[3]/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/input'
 driver.find_element_by_xpath(login_button).click()
 
-# [수집 데이터 속성]
+# [검색 페이지 표시]
 # 1. 실시간 통계 페이지 접속
 url_realtime = 'https://www.airportal.go.kr/knowledge/statsnew/realtime/airlineRoute.jsp#'
 driver.get(url_realtime)
-
 # 2. 제주 노선 설정
-from selenium.webdriver.support.select import Select
-
 selectRoute = Select(driver.find_element_by_name('seah_ad'))
 selectRoute.select_by_visible_text('제주')
 
-# [데이터 출력]
+# [필요 함수 및 패스 지정]
 # 1. 기간 설정
-from datetime import date, timedelta
-import time
-
-# 기간 설정 함수
+# 날짜 함수
 def dateRange(st_date, end_date):
     for n in range(int((end_date - st_date).days)):
         yield st_date + timedelta(n)
-
-# 날짜 검색 함수
+# 검색 함수
 def dateSet(className, d):
     from selenium.webdriver.common.keys import Keys
     driver.find_element_by_name(className).clear()
@@ -45,8 +45,8 @@ def dateSet(className, d):
     driver.find_element_by_name(className).send_keys(str(d))
     driver.find_element_by_name(className).send_keys(Keys.ENTER)
 
-# 데이터 추출
-# 행이 존재하는지 확인하는 함수
+# 2. 데이터 로드 관련
+# 셀 존재 여부 판별
 from selenium.common.exceptions import NoSuchElementException
 def check(xpath):
     try:
@@ -54,27 +54,18 @@ def check(xpath):
     except NoSuchElementException:
         return False
     return True
-
+# 스크롤
 def scroll(i):
     scroll_tbody = driver.find_element_by_xpath('//*[@id="mySheet-table"]/tbody')
     itemlist = scroll_tbody.find_element_by_xpath('//*[@id="mySheet-table"]/tbody/tr[2]/td[2]/div/div')
     driver.execute_script("arguments[0].scrollBy(0,"+str(i)+")", itemlist)
 
-
-# csv파일 생성
-import csv
-f = open('airportalData.csv', 'a', newline='')
-wr = csv.writer(f)
-
-#시작일(st_date), 종료일(end_date) 입력
-st_date = date(2020, 12, 30)
-end_date = date(2021, 1, 3)
-
+# 4. 표 경로 지정
 tbody_xpath = '//*[@id="mySheet-table"]/tbody/tr[3]/td/div/div[1]/table/tbody'
 trSum_xpath = '//*[@id="mySheet-table"]/tbody/tr[2]/td[1]/div/table/tbody/tr[4]'
 tbody = driver.find_element_by_xpath(tbody_xpath)
 
-# tr 길이 알아내기
+# 전체 행 길이 알아내기
 def trLen():
     i = 2
     tr_count = 0
@@ -89,6 +80,7 @@ def trLen():
             break
     return tr_count
 
+# 합계열 불러오기
 def dataSum():
     dataSum_xpath = '//*[@id="mySheet-table"]/tbody/tr[2]/td[1]/div/table/tbody/tr[4]'
     dataSum_tr = driver.find_element_by_xpath(dataSum_xpath)
@@ -102,15 +94,25 @@ def dataSum():
     wr.writerow(dataSum_list)
     print('합계 저장 완료')
 
-# 반복
-# 날짜검색-합계행저장-데이터행갯수검색-데이터행저장 순으로 반복하는 구조
+"""
+3. 날짜 지정(직접 입력 필요)
+#시작일(st_date), 종료일(end_date) 입력하세요. 종료일의 경우 원하시는 날짜에 하루를 더한 값을 넣으셔야 합니다.
+#예: 2020년 8월 31일까지 검색할 경우, 2020, 9, 1로 입력
+"""
+st_date = date(2020, 12, 30)
+end_date = date(2021, 1, 3)
+
+# 데이터 수집 반복문
+# 날짜검색-합계행저장-데이터행판별-데이터행저장+스크롤 순으로 반복하는 구조
 for single_date in dateRange(st_date, end_date):
+    # 날짜 검색
     date = single_date.strftime("%Y%m%d")
     dateSet('current_dt_from', date)
     dateSet('current_dt_to', date)
     print(date + ' 검색합니다.')
     searchData = '//*[@id="realContents"]/div/div[2]/div[1]/div[1]/a[2]'
     driver.find_element_by_xpath(searchData).click()
+    time.sleep(3)
 
     # 합계 행 저장
     dataSum()
@@ -122,10 +124,9 @@ for single_date in dateRange(st_date, end_date):
     for i in range(2, tr_ct, 1):
         tr_xpath = tbody_xpath + '/tr[' + str(i) + ']'
         data_tr = driver.find_element_by_xpath(tr_xpath)
-        td_14_xpath = tr_xpath + '/td[14]'
-
+        td_14_xpath = tr_xpath + '/td[14]' #노선탭 병합 여부를 판별하는데 필요
         # 데이터 추출
-        if check(td_14_xpath) != 0:
+        if check(td_14_xpath) != 0: #동일 노선 내 첫번째 행인 경우
             # 노선 넣기
             tdDep_xpath = tr_xpath + '/td[3]'
             td_dep = data_tr.find_element_by_xpath(tdDep_xpath)
@@ -142,8 +143,8 @@ for single_date in dateRange(st_date, end_date):
                 data_list.append(data)
             wr.writerow(data_list)
             scroll(30)
-        else:
-            # 노선 넣기
+        else: # 동일 노선으로 병합된 행인 경우
+            # 앞에서 받아온 병합된 노선명을 사전에 리스트로 입력
             data_list = [date, dep, arv]
             # 데이터 추가
             for n in range(2, 12, 1):
@@ -154,4 +155,3 @@ for single_date in dateRange(st_date, end_date):
             wr.writerow(data_list)
             scroll(30)
     print('저장 완료')
-
